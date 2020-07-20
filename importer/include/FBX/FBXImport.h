d8f2f5ef1058ef3950352036b0c521576930f0df
WinteryFox <dragonhel5@gmail.com>
2020-07-20 15:45:53 +0200

    change shared_ptr to unique_ptr

--- importer/include/FBX/FBXImport.h
+++ importer/include/FBX/FBXImport.h
@@ -5,7 +5,7 @@
 #include "Vector3.h"
 
 namespace FBX {
-    static std::shared_ptr<const Scene> importFile(const std::string &path, int processes) {
+    static std::unique_ptr<const Scene> importFile(const std::string &path, int processes) {
         Decoder decoder(path);
         auto root = decoder.readFile();
 
