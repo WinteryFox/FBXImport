6036e7db082157744142168aaa8b2fb565abc58c
WinteryFox <dragonhel5@gmail.com>
2020-07-20 15:43:45 +0200

    remove unimplemented processes

--- importer/include/FBX/Process.h
+++ importer/include/FBX/Process.h
@@ -3,9 +3,5 @@
 namespace FBX {
     enum Process {
         TRIANGULATE = 0x001,
-        GENERATE_UVS = 0x002,
-        MAKE_X_UP = 0x004,
-        MAKE_Y_UP = 0x008,
-        MAKE_Z_UP = 0x0010,
     };
 }
